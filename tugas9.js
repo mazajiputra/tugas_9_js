22ea5cd68c9b993d0fcafe6dba56ed98b5079765
mazajiputra <mazajiputra@gmail.com>
2021-10-29 23:45:43 +0700

    Kumpul Tugas

---
+++ tugas9.js
@@ -0,0 +1,13 @@
+function tugas(params) {
+    
+    var siswa = {
+        topik        : "Matematika-1",
+        namaSiswa    : "Tono",
+        nilai        : 98,
+        catatan      : "Luar biasa",
+    }
+    for(var x in siswa){
+        console.log(siswa[x])
+    }
+}
+tugas()
